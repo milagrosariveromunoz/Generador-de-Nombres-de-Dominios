94cc5c43bb8990ee1d02b17bcc3aab0a56f301f3
milagrosariveromunoz <milagrosarivero@icloud.com>
2023-01-13 19:44:13 +0000

    resuelto

--- src/app.js
+++ src/app.js
@@ -7,5 +7,35 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  var pronoun = ['the','our' ];
+  var adj = ['great','big'];
+  var noun =['jogger','racoon'];
+  var arr = [];
+  var li1 = '<li class="list-group-item">';
+  var li2 = '</li>';
+  let textHTML = '<ul>';
+  textHTML += '<li>linea</li>'
+
+
+  //1.Combinaciones de los arreglos
+  //2.Lista combinaciones HTML
+  pronoun.forEach(e => {
+    adj.forEach(e1 => {
+      noun.forEach(e2 => {
+        let arr2 = [];
+        arr2.push(li1 + e + e1 + e2 + li2);
+        arr.push(arr2);
+      });
+    });
+  });
+  var texto = "";
+  arr.forEach(a=> {
+   texto+=a; 
+  }
+    )
+    
+
+  document.querySelector("#lista").innerHTML = texto
+  
 };
+
